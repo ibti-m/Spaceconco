014868a1900807b7b061baceaeba2e4558c5ce64
ibti-m <ibtihalmossa@gmail.com>
2025-09-17 15:25:47 -0400

    Add ROS2 Python & C++ publisher/subscriber packages

---
+++ src/my_py_pkg/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'rclpy'],
+    zip_safe=True,
+    maintainer='ibti',
+    maintainer_email='ibti@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = my_py_pkg.publisher_node:main',
+        'listener = my_py_pkg.subscriber_node:main',
+        ],
+    },
+)
